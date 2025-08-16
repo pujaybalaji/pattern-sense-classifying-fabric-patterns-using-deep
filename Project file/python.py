543193cb66e6cdc6ce65d313b4c7637825061ae2
pujaybalaji <228x1a0495@khitguntur.ac.in>
2025-08-16 19:31:01 +0530

    Delete Project file directory

--- Project file/python.py
+++
@@ -1,9 +0,0 @@
-import flask
-import numpy
-import pandas
-import tensorflow as tf
-import keras
-import matplotlib
-
-print("✅ All packages are installed successfully.")
-print("✅ TensorFlow version:", tf.__version__)
